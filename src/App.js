32b28b1960458218049b8fce98ef25f92fc72f89
Jelena Borotic <Jelena.Borotic@infobip.com>
2019-11-17 20:40:37 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I Was changed 3
         </a>
       </header>
     </div>
